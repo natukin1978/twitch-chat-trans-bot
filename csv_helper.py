537bb6b1b4f5d1700790af27dd6ec301434b508f
natukin1978 <natukin1978@hotmail.com>
2025-02-25 01:50:38 +0900

    There is no need to use typing, so I fixed it.

--- csv_helper.py
+++ csv_helper.py
@@ -1,11 +1,10 @@
 import csv
 import os
-from typing import Any, List
 
 import global_value as g
 
 
-def read_csv_to_list(name: str) -> List[List[Any]]:
+def read_csv_to_list(name: str) -> list[list[any]]:
     if not os.path.isabs(name):
         name = os.path.join(g.base_dir, name)
     if not os.path.isfile(name):
