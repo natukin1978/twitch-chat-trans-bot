d4678bd7e7c57fa99018bf06969e92d4527d43d3
natukin1978 <natukin1978@hotmail.com>
2026-04-11 23:13:32 +0900

    The readability was improved by using JOIN to combine the elements.

--- constants.py
+++ constants.py
@@ -1,2 +1,7 @@
-CALLBACK_URL_BOT = "http://localhost:4343/oauth?scopes=user:read:chat%20user:write:chat%20user:bot&force_verify=true"
+CALLBACK_URL_BOT = "http://localhost:4343/oauth?scopes=" + "%20".join([
+    "user:read:chat",
+    "user:write:chat",
+    "user:bot",
+]) + "&force_verify=true"
+
 CALLBACK_URL_OWNER = "http://localhost:4343/oauth?scopes=channel:bot&force_verify=true"
